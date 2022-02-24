a99d143051234eaea3b0a3e3b23c5c31142aa407
Vincent Cradler <vvvv@Vincents-MacBook-Pro-2.local>
2022-02-24 14:05:53 -0500

    stuff

--- A-an-Example.py
+++ A-an-Example.py
@@ -1,5 +1,23 @@
-# Open file in read mode
-a = open("a_an_example.in.txt", 'r')
+with open ("a_an_example.in.txt","r") as f:
+    people, proj = f.readline().split(" ")
+    print(f"people: {people} proj: {proj}")
+    contrib = {}
+    proj_info = {}
+    for i in range(int(people)):
+        name, skills = f.readline().split(" ")
+        contrib[name] = {}
+        for i in range(int(skills)):
+            skill, lvl = f.readline().split(" ")
+            contrib[name][skill] = int(lvl)
 
-# prints the entire file
-print(a.read())
+    for i in range(int(proj)):
+        proj_name, days, score, best_before, roles = f.readline().split(" ")
+        proj_info[proj_name] = [int(days), int(score), int(best_before), int(roles), {}]
+        
+        for role in range(int(roles)):
+            skill, lvl = f.readline().split(" ")
+            proj_info[proj_name][4][skill] = int(lvl)
+
+    print(contrib) 
+    print(proj_info)
+    f.close()        
